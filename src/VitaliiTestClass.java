4e69ab00a29b5aee282fdcd8e06f9c8d8ea57878
VitaliiK <whitetech0047@gmail.com>
2025-03-18 22:15:00 +0100

    Доработал функционалgit add .!

--- src/VitaliiTestClass.java
+++ src/VitaliiTestClass.java
@@ -2,6 +2,5 @@ public class VitaliiTestClass {
     private static void testMethod(){
         System.out.println("Тестовый метод!!!");
         System.out.println("Тестовый метод!!!");
-        System.out.println("Тестовый метод!!!");
     }
 }
